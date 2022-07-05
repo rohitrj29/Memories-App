3019b5b89897a145baa05811cc1de05fa5892b1a
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-07-05 14:28:33 +0530

    on it

--- server/index.js
+++ server/index.js
@@ -7,11 +7,13 @@ import postRoutes from './routes/posts.js'
 
 const app= express();
 
-app.use('/posts',postRoutes);
+
 
 app.use(bodyParser.json({limit:"30mb",extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended: true}));
 app.use(cors());
+app.use('/posts',postRoutes);
+
 const CONNECTION_URL = 'mongodb+srv://javascriptmastery:js123!!@cluster0.ghi6t.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true})
